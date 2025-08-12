52812931ff759de23e865937bc4c711fe64006ed
doracoin <doracoin@foxmail.com>
2025-08-12 10:31:53 +0800

    Make some commits.

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    kotlin("jvm") version "2.1.21"
+}
+
+group = "org.jer"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(17)
+}
